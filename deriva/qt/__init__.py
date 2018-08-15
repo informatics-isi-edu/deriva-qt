cc893e7ce0f3911cd14de6c52fe12543aab69a4e
mikedarcy <mikedarcy@users.noreply.github.com>
2018-08-15 11:23:35 -0700

    Add feature to Auth-Agent to write out CURL-compatible cookie jar file to ""~/.deriva/cookies.txt".

--- deriva/qt/__init__.py
+++ deriva/qt/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 from deriva.qt.common.async_task import async_execute, AsyncTask, Request
 from deriva.qt.common.log_widget import QPlainTextEditLogger
